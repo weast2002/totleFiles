893408b02dddd43cd81ace1b667a96d379bc13af
wjd <wjd@batter.com>
2012-07-25 10:40:42 +0800

    Signed-off-by: wjd <wjd@batter.com>

--- Git_dev/src/Test.java
+++ Git_dev/src/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         int a = 1;
-        double c = 22333;
+        double c = 99988877.33;
         String ssString = "vvvvvvcxvsfsdf";
     }
 
